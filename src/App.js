7b99b336eba73654f1d2ef0086662808ba5f6a39
André Junior <ajjunior33@gmail.com>
2020-02-16 14:00:55 -0300

    Alteraçoes

--- src/App.js
+++ src/App.js
@@ -8,14 +8,6 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1> GH-Pages AndreJr</h1>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
