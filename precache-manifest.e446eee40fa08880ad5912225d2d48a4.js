fc3d962b59fdac78c583a92571a85889f658d208
André Junior <ajjunior33@gmail.com>
2020-02-16 13:43:11 -0300

    Updates

---
+++ precache-manifest.e446eee40fa08880ad5912225d2d48a4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3dd9b20b308f20a43d74922e73e857e8",
+    "url": "/gh-pages-teste/index.html"
+  },
+  {
+    "revision": "b6d791944637ca5035f0",
+    "url": "/gh-pages-teste/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "ad2c8c0124b45241d2cd",
+    "url": "/gh-pages-teste/static/js/2.b9784fa3.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/gh-pages-teste/static/js/2.b9784fa3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b6d791944637ca5035f0",
+    "url": "/gh-pages-teste/static/js/main.54206bb7.chunk.js"
+  },
+  {
+    "revision": "1eff41d6fbbf480388bf",
+    "url": "/gh-pages-teste/static/js/runtime-main.4bb4004e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/gh-pages-teste/static/media/logo.5d5d9eef.svg"
+  }
+]);
